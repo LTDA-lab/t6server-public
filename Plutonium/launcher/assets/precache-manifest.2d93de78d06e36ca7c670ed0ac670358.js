1d17533d7a6b5adb5e9f77f905cf4d21016a16b8
LTDA-lab <smarchivos3@gmail.com>
2024-12-22 07:02:58 -0300

    Initial commit

---
+++ Plutonium/launcher/assets/precache-manifest.2d93de78d06e36ca7c670ed0ac670358.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a9984fe751bb5250c1",
+    "url": "css/app.5b0a1e7d.css"
+  },
+  {
+    "revision": "1db33393287b08b651af",
+    "url": "css/auth.4a6b27be.css"
+  },
+  {
+    "revision": "3e7bd25f7ab79efb263d",
+    "url": "css/chunk-11e7bc78.a8fa0530.css"
+  },
+  {
+    "revision": "32f058dcacc7c8ec9c93",
+    "url": "css/chunk-4f6208b2.def6a674.css"
+  },
+  {
+    "revision": "e68241c11dfe686e5120",
+    "url": "css/chunk-520ec366.3f565a45.css"
+  },
+  {
+    "revision": "64d7930daade3538fae7",
+    "url": "css/chunk-d15685cc.29ddedaf.css"
+  },
+  {
+    "revision": "94237cc6dd86c40ca16b",
+    "url": "css/games.b0791aa4.css"
+  },
+  {
+    "revision": "849d500f76d7850d3a46058e457ed5a5",
+    "url": "img/avatar-default.849d500f.jpg"
+  },
+  {
+    "revision": "fa555ecb0c62c09f285090ad08223a83",
+    "url": "img/iw5.fa555ecb.jpg"
+  },
+  {
+    "revision": "804ee0068586c992ba3b54bf66995971",
+    "url": "img/iw5_icon.804ee006.png"
+  },
+  {
+    "revision": "9bd3a45dbbc3f8d6452e720e38d886b5",
+    "url": "img/iw5_logo.9bd3a45d.png"
+  },
+  {
+    "revision": "62db7e114bbcdb7eddb6aac2bce0ec51",
+    "url": "img/login_bg.62db7e11.png"
+  },
+  {
+    "revision": "e77924d1eb12ac9e6c9d93d718a987b3",
+    "url": "img/t4mp.e77924d1.jpg"
+  },
+  {
+    "revision": "84a265b71adf15ada388eb8fcb2ea64c",
+    "url": "img/t4mp_icon.84a265b7.png"
+  },
+  {
+    "revision": "59e4aebdb4b97380adc358981ab29add",
+    "url": "img/t4mp_logo.59e4aebd.png"
+  },
+  {
+    "revision": "20d703034854d0fe7c5206920f01c0ab",
+    "url": "img/t4sp.20d70303.jpg"
+  },
+  {
+    "revision": "1e1987d84038aae38610cab9c999868d",
+    "url": "img/t4sp_icon.1e1987d8.png"
+  },
+  {
+    "revision": "3b95821fdcfda7fddfa9c7deb752b0d7",
+    "url": "img/t4sp_logo.3b95821f.png"
+  },
+  {
+    "revision": "a4c26415540278cc3c9886d360819716",
+    "url": "img/t5mp.a4c26415.jpg"
+  },
+  {
+    "revision": "215e7d942e2aec310bb6a617fa6a523f",
+    "url": "img/t5mp_icon.215e7d94.png"
+  },
+  {
+    "revision": "ab399f8ffd1d1bd26ba2af1f8f6ade8c",
+    "url": "img/t5mp_logo.ab399f8f.png"
+  },
+  {
+    "revision": "463f63afdb30d8193d8288e8474db8a2",
+    "url": "img/t5sp.463f63af.jpg"
+  },
+  {
+    "revision": "f951b0ced18db7f719c74cc4d98ec0f9",
+    "url": "img/t5sp_icon.f951b0ce.png"
+  },
+  {
+    "revision": "923e41065e3995b2b8a44265f12523b7",
+    "url": "img/t5sp_logo.923e4106.png"
+  },
+  {
+    "revision": "bf54e4370841e6353563854140089e1a",
+    "url": "img/t6mp.bf54e437.jpg"
+  },
+  {
+    "revision": "e4fc734b7e536d945d9fafaea2ea8e52",
+    "url": "img/t6mp_logo.e4fc734b.png"
+  },
+  {
+    "revision": "6ff8e1f7c0889c0ef56394ccb8cf67e3",
+    "url": "img/t6zm.6ff8e1f7.jpg"
+  },
+  {
+    "revision": "992fa331f512b4cb45b83e0720a9a5c5",
+    "url": "img/t6zm_icon.992fa331.png"
+  },
+  {
+    "revision": "6935c7f41ac75f5a14882b4a48bf7143",
+    "url": "img/t6zm_logo.6935c7f4.png"
+  },
+  {
+    "revision": "fc46fd73d250330c942990dfb2e3f069",
+    "url": "index.html"
+  },
+  {
+    "revision": "b1a9984fe751bb5250c1",
+    "url": "js/app.d4d9a1c6.js"
+  },
+  {
+    "revision": "1db33393287b08b651af",
+    "url": "js/auth.688202d0.js"
+  },
+  {
+    "revision": "3e7bd25f7ab79efb263d",
+    "url": "js/chunk-11e7bc78.2d0f245a.js"
+  },
+  {
+    "revision": "32f058dcacc7c8ec9c93",
+    "url": "js/chunk-4f6208b2.22635986.js"
+  },
+  {
+    "revision": "e68241c11dfe686e5120",
+    "url": "js/chunk-520ec366.59e39e11.js"
+  },
+  {
+    "revision": "64d7930daade3538fae7",
+    "url": "js/chunk-d15685cc.589056ed.js"
+  },
+  {
+    "revision": "a86b0bef0e10c297ab3f",
+    "url": "js/chunk-vendors.d77b8b8e.js"
+  },
+  {
+    "revision": "94237cc6dd86c40ca16b",
+    "url": "js/games.2559cd9a.js"
+  },
+  {
+    "revision": "515e59f16e9296205663258d761e2471",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "644c032e7a0f872e8f1b34598b3db70d",
+    "url": "nix-bridge.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
---
+++ README.md
@@ -0,0 +1,5 @@
+# T6ServerConfigs
+This repo contains the T6 Dedicated Server Config for PlutoniumT6. Learn more by visiting the [Plutonium Documentation](https://plutonium.pw/docs/server/t6/setting-up-a-server/)
+
+Or one of our communities tutorial's
+* [AWOG Community Forum](https://forum.awog.at/topic/32/)
